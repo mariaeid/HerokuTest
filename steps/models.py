3a76d92e81fe9346bff68820b92ebdbe867de1e3
mariaeid <maria.eidland@gmail.com>
2019-11-16 12:04:30 +0100

    Changed search path

--- steps/models.py
+++ steps/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
-from django.db import models
+import os
+from django.conf import settings
 
 class Steps(models.Model):
   number = models.IntegerField()
   title = models.CharField(max_length=120)
   description = models.TextField()
-  icon = models.ImageField(upload_to='images/', null=True)
+  icon = models.ImageField(upload_to=os.path.join(settings.MEDIA_ROOT, 'images/'), null=True)
 
   def _str_(self):
     return self.title
